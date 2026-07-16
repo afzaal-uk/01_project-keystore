9f879411fc253677137589489b61ff3bb45b3332
afzaal-uk <afzaal.uk798@gmail.com>
2026-07-16 10:01:51 +0100

    Delete sql/18_revenue_vs_profit_by_category.sql

--- sql/18_revenue_vs_profit_by_category.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    PRODGRP1 AS category,
-    SUM(SELL * QTY) AS total_revenue,
-    SUM((SELL - COST) * QTY) AS total_profit
-FROM SALES_HISTORY
-GROUP BY PRODGRP1
-ORDER BY total_revenue DESC;
