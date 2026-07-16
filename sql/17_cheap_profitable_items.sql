ded92eebffd855a828f7c709c7a98516b262dfc7
afzaal-uk <afzaal.uk798@gmail.com>
2026-07-16 10:01:41 +0100

    Delete sql/17_cheap_profitable_items.sql

--- sql/17_cheap_profitable_items.sql
+++
@@ -1,9 +0,0 @@
-SELECT FIRST 10
-	s_desc AS product,
-	sell,
-	sum((sell - cost) * qty) AS total_profit,
-	sum(qty) AS units_sold
-FROM SALES_HISTORY 
-WHERE sell < 2 AND sell > 0
-GROUP BY s_desc, sell
-ORDER BY total_profit DESC;
