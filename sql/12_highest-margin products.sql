be00ff52ba72d13b668537661f6dc14d85cfed91
afzaal-uk <afzaal.uk798@gmail.com>
2026-07-16 10:01:10 +0100

    Delete sql/12_highest-margin products.sql

--- sql/12_highest-margin products.sql
+++
@@ -1,9 +0,0 @@
-SELECT FIRST 10
-	S_DESC AS product,
-	sum((sell - cost) * QTY) AS total_profit,
-	sum(sell * qty) AS total_revenue,
-	(sum((sell - cost) * qty) / sum(sell * qty)) * 100 AS margin_percent
-FROM SALES_HISTORY 
-GROUP BY S_DESC
-HAVING sum(qty) > 100 AND sum(sell * qty) > 0
-ORDER BY margin_percent desc;
